3c29139f577829772f01b047fd77fd21cc9447b9
alexarcombe <arcombe.a@gmail.com>
2020-05-23 08:56:35 +1200

    refactor tests for useReducer

--- VehicleManagerSystem/ClientApp/src/hooks/useFilterSearch.test.js
+++ VehicleManagerSystem/ClientApp/src/hooks/useFilterSearch.test.js
@@ -1,153 +1,307 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import useFilterSearch from './useFilterSearch';
+import { SET_LIST, CHANGE, ADD, UPDATE, REMOVE } from '../actions/types';
 
 const initialValues = {
   searchPhrase: '',
   filter: 'name',
   list: [
-    { name: 'Coretex Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
-    { name: 'Alex Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '1',
+      name: 'Coretex Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
   ],
   filteredList: [
-    { name: 'Coretex Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
-    { name: 'Alex Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '1',
+      name: 'Coretex Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
   ],
 };
 
-test('should use filter search', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
-
-  const [values, onChange] = result.current;
-  expect(values).toEqual(initialValues);
-  expect(typeof onChange).toBe('function');
+let result, state, dispatch;
+beforeEach(() => {
+  const res = renderHook(() => useFilterSearch(initialValues));
+  result = res.result;
+  [state, dispatch] = result.current;
 });
 
-test('should be able to change list', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
+test('should use filter search', () => {
+  expect(state).toEqual(initialValues);
+  expect(typeof dispatch).toBe('function');
+});
 
-  let [_, onChange, setList] = result.current;
-
+test('should do nothing on default dispatch', () => {
   act(() => {
-    setList([
-      { name: 'Vodafone Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
-      {
-        name: 'A Cool Truck',
-        model: 'Boxcar 0.1.9',
-        tags: ['SWE', 'Stockholm'],
-      },
-    ]);
+    dispatch({});
+  });
+  [state, dispatch] = result.current;
+  expect(state).toEqual(initialValues);
+  expect(typeof dispatch).toBe('function');
+});
+
+const newList = [
+  {
+    id: '3',
+    name: 'Vodafone Truck',
+    model: 'Tesla x.x',
+    tags: ['NZ', 'Auckland'],
+  },
+  {
+    id: '4',
+    name: 'A Cool Truck',
+    model: 'Boxcar 0.1.9',
+    tags: ['SWE', 'Stockholm'],
+  },
+];
+
+test('should be able to change list', () => {
+  act(() => {
+    dispatch({ type: SET_LIST, payload: newList });
   });
 
-  const { searchPhrase, filter, list, filteredList } = result.current[0];
+  const [{ searchPhrase, filter, list, filteredList }] = result.current;
   expect(searchPhrase).toBe('');
   expect(filter).toBe('name');
   expect(list).toEqual([
-    { name: 'Vodafone Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
-    { name: 'A Cool Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '3',
+      name: 'Vodafone Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '4',
+      name: 'A Cool Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
   ]);
   expect(filteredList).toEqual([
-    { name: 'Vodafone Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
-    { name: 'A Cool Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '3',
+      name: 'Vodafone Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '4',
+      name: 'A Cool Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
   ]);
 });
 
 test('should filter by name "alex"', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
-
-  const [_, onChange] = result.current;
-
   act(() => {
-    onChange({ target: { name: 'searchPhrase', value: 'alex' } });
+    dispatch({
+      type: CHANGE,
+      payload: { name: 'searchPhrase', value: 'alex' },
+    });
   });
 
-  const { searchPhrase, filteredList } = result.current[0];
+  const [{ searchPhrase, filteredList }] = result.current;
   expect(searchPhrase).toBe('alex');
   expect(filteredList).toEqual([
-    { name: 'Alex Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
   ]);
 });
 
 test('should filter by model "x.x"', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
-
-  let [_, onChange] = result.current;
-
   act(() => {
-    onChange({ target: { name: 'filter', value: 'model' } });
+    dispatch({ type: CHANGE, payload: { name: 'filter', value: 'model' } });
+    dispatch({
+      type: CHANGE,
+      payload: { name: 'searchPhrase', value: 'Tesla' },
+    });
   });
 
-  [_, onChange] = result.current;
-
-  act(() => {
-    onChange({ target: { name: 'searchPhrase', value: 'Tesla' } });
-  });
-
-  const { searchPhrase, filter, filteredList } = result.current[0];
+  const [{ searchPhrase, filter, filteredList }] = result.current;
   expect(searchPhrase).toBe('Tesla');
   expect(filter).toBe('model');
   expect(filteredList).toEqual([
-    { name: 'Coretex Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
+    {
+      id: '1',
+      name: 'Coretex Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
   ]);
 });
 
 test('should filter by tag "SWE"', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
-
-  let [_, onChange] = result.current;
-
   act(() => {
-    onChange({ target: { name: 'filter', value: 'tags' } });
+    dispatch({ type: CHANGE, payload: { name: 'searchPhrase', value: 'SWE' } });
+    dispatch({ type: CHANGE, payload: { name: 'filter', value: 'tags' } });
   });
 
-  [_, onChange] = result.current;
-
-  act(() => {
-    onChange({ target: { name: 'searchPhrase', value: 'SWE' } });
-  });
-
-  const { searchPhrase, filter, filteredList } = result.current[0];
+  const [{ searchPhrase, filter, filteredList }] = result.current;
   expect(searchPhrase).toBe('SWE');
   expect(filter).toBe('tags');
   expect(filteredList).toEqual([
-    { name: 'Alex Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
   ]);
 });
 
 test('should find nothing on filter by name "Arcombe"', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
-
-  const [_, onChange] = result.current;
-
   act(() => {
-    onChange({ target: { name: 'searchPhrase', value: 'Arcombe' } });
+    dispatch({
+      type: CHANGE,
+      payload: { name: 'searchPhrase', value: 'Arcombe' },
+    });
   });
 
-  const { searchPhrase, filter, filteredList } = result.current[0];
+  const [{ searchPhrase, filter, filteredList }] = result.current;
   expect(searchPhrase).toBe('Arcombe');
   expect(filter).toBe('name');
   expect(filteredList).toEqual([]);
 });
 
 test('should be able to go back to original list without search phrase', () => {
-  const { result } = renderHook(() => useFilterSearch(initialValues));
-
-  let [_, onChange] = result.current;
-
   act(() => {
-    onChange({ target: { name: 'searchPhrase', value: 'Arcombe' } });
-  });
-
-  [_, onChange] = result.current;
-
-  act(() => {
-    onChange({ target: { name: 'searchPhrase', value: '' } });
+    dispatch({
+      type: CHANGE,
+      payload: { name: 'searchPhrase', value: 'Arcombe' },
+    });
+    dispatch({
+      type: CHANGE,
+      payload: { name: 'searchPhrase', value: '' },
+    });
   });
 
   const { searchPhrase, filter, filteredList } = result.current[0];
   expect(searchPhrase).toBe('');
   expect(filter).toBe('name');
   expect(filteredList).toEqual([
-    { name: 'Coretex Truck', model: 'Tesla x.x', tags: ['NZ', 'Auckland'] },
-    { name: 'Alex Truck', model: 'Boxcar 0.1.9', tags: ['SWE', 'Stockholm'] },
+    {
+      id: '1',
+      name: 'Coretex Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
+  ]);
+});
+
+test('should add new items', () => {
+  act(() => {
+    dispatch({
+      type: ADD,
+      payload: {
+        id: '3',
+        name: 'Vodafone Truck',
+        model: 'Tesla x.x',
+        tags: ['NZ', 'Auckland'],
+      },
+    });
+  });
+
+  const { searchPhrase, filter, filteredList } = result.current[0];
+  expect(searchPhrase).toBe('');
+  expect(filter).toBe('name');
+  expect(filteredList).toEqual([
+    {
+      id: '1',
+      name: 'Coretex Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
+    {
+      id: '3',
+      name: 'Vodafone Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+  ]);
+});
+
+test('should update "Coretext Truck" to "Coretex Car"', () => {
+  act(() => {
+    dispatch({
+      type: UPDATE,
+      payload: {
+        id: '1',
+        name: 'Coretex Car',
+        model: 'Tesla x.x',
+        tags: ['NZ', 'Auckland'],
+      },
+    });
+  });
+
+  const { searchPhrase, filter, filteredList } = result.current[0];
+  expect(searchPhrase).toBe('');
+  expect(filter).toBe('name');
+  expect(filteredList).toEqual([
+    {
+      id: '1',
+      name: 'Coretex Car',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
+    {
+      id: '2',
+      name: 'Alex Truck',
+      model: 'Boxcar 0.1.9',
+      tags: ['SWE', 'Stockholm'],
+    },
+  ]);
+});
+
+test('should remove id 2', () => {
+  act(() => {
+    dispatch({
+      type: REMOVE,
+      payload: '2',
+    });
+  });
+
+  const { searchPhrase, filter, filteredList } = result.current[0];
+  expect(searchPhrase).toBe('');
+  expect(filter).toBe('name');
+  expect(filteredList).toEqual([
+    {
+      id: '1',
+      name: 'Coretex Truck',
+      model: 'Tesla x.x',
+      tags: ['NZ', 'Auckland'],
+    },
   ]);
 });
--- VehicleManagerSystem/ClientApp/src/hooks/useFormFields.test.js
+++ VehicleManagerSystem/ClientApp/src/hooks/useFormFields.test.js
@@ -1,5 +1,6 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import useFormFields from './useFormFields';
+import { SET_FIELDS, SET_ERRORS, SET_MODE, CHANGE } from '../actions/types';
 
 const initialValues = {
   name: '',
@@ -12,14 +13,31 @@ const initialValues = {
   comments: [],
 };
 
-test('should use form fields', () => {
-  const { result } = renderHook(() => useFormFields({ initialValues }));
+let result, dispatch;
+beforeEach(() => {
+  const res = renderHook(() => useFormFields(initialValues));
+  result = res.result;
+  [, dispatch] = result.current;
+});
 
-  const [values, handleChange, setFormFields] = result.current;
+test('should use form fields', () => {
+  const [{ values, mode, errors }, dispatch] = result.current;
 
   expect(values).toEqual(initialValues);
-  expect(typeof handleChange).toBe('function');
-  expect(typeof setFormFields).toBe('function');
+  expect(mode).toEqual('Create');
+  expect(errors).toEqual({});
+  expect(typeof dispatch).toBe('function');
+});
+
+test('should do nothing on default dispatch', () => {
+  act(() => {
+    dispatch({});
+  });
+  const [{ values, mode, errors }] = result.current;
+  expect(values).toEqual(initialValues);
+  expect(mode).toEqual('Create');
+  expect(errors).toEqual({});
+  expect(typeof dispatch).toBe('function');
 });
 
 const updatedValues = {
@@ -40,50 +58,70 @@ const updatedValues = {
 };
 
 test('should update values', () => {
-  const { result } = renderHook(() => useFormFields({ initialValues }));
-
-  const [_, handleChange, setFormFields] = result.current;
-
   act(() => {
     for (let [key, value] of Object.entries(updatedValues)) {
-      handleChange({ target: { id: key, value: value } });
+      dispatch({ type: CHANGE, payload: { id: key, value: value } });
     }
   });
 
-  const [updated] = result.current;
-
-  expect(updated).toEqual(updatedValues);
+  const { values } = result.current[0];
+  expect(values).toEqual(updatedValues);
 });
 
-test('should set all form values', () => {
-  const { result } = renderHook(() => useFormFields({ initialValues }));
-
-  const [values, handleChange, setFormFields] = result.current;
-
+test('should set all form fields', () => {
   act(() => {
-    setFormFields(updatedValues);
+    dispatch({
+      type: SET_FIELDS,
+      payload: { values: updatedValues, mode: 'Create' },
+    });
   });
 
-  const [updated] = result.current;
-  expect(updated).toEqual(updatedValues);
+  const { values } = result.current[0];
+  expect(values).toEqual(updatedValues);
 });
 
 test('should reset form values', () => {
-  const { result } = renderHook(() => useFormFields({ initialValues }));
-
-  const [_, handleChange, setFormFields] = result.current;
-
   act(() => {
-    handleChange({ target: { id: 'name', value: 'Truck' } });
+    dispatch({ type: CHANGE, payload: { id: 'name', value: 'Truck' } });
   });
 
-  const [updated] = result.current;
-  expect(updated.name).toBe('Truck');
+  const {
+    values: { name },
+  } = result.current[0];
+  expect(name).toBe('Truck');
 
   act(() => {
-    setFormFields({});
+    dispatch({
+      type: SET_FIELDS,
+      payload: { values: initialValues, mode: 'Create' },
+    });
   });
 
-  const [reset] = result.current;
-  expect(reset).toEqual(initialValues);
+  const { values } = result.current[0];
+  expect(values).toEqual(initialValues);
+});
+
+test('should set mode to Selected', () => {
+  act(() => {
+    dispatch({ type: SET_MODE, payload: 'Selected' });
+  });
+  const { mode } = result.current[0];
+  expect(mode).toBe('Selected');
+});
+
+test('should set errors', () => {
+  act(() => {
+    dispatch({
+      type: SET_ERRORS,
+      payload: {
+        name: 'name needs to be provided',
+        speed: "speed can't be negative",
+      },
+    });
+  });
+  const { errors } = result.current[0];
+  expect(errors).toEqual({
+    name: 'name needs to be provided',
+    speed: "speed can't be negative",
+  });
 });
